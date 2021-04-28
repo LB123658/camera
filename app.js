672f726306889d112af83dde0057cab8b379c176
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-04-27 19:13:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,3 @@ document.getElementById("green").style.visibility = "visible";
 function exito() {
 document.getElementById("green").style.visibility = "hidden";
 }
-function print() {
-	print(window.location);
-}
